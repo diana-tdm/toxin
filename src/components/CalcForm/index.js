45c03dc556018654a0c3d66b49523b6a7d386796
Diana <sun_ray@mail.ru>
2021-07-24 15:50:52 +0500

    RoomDetails сверстала калькулятор, изменила компонент Button

--- src/components/CalcForm/index.js
+++ src/components/CalcForm/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
 import Button from "../UI/Button";
+
 import "./style.scss";
 
 // markup
@@ -9,7 +10,8 @@ const CalcForm = () => {
     <form className="calc-form">
       <div className="calc-form__info">
         <div className="calc-form__number">
-          № 888 <span className="calc-form__type">люкс</span>
+          <span className="calc-form__element">№</span> 888{" "}
+          <span className="calc-form__type">люкс</span>
         </div>
         <div className="calc-form__price">
           9 990₽ <span className="calc-form__time">в сутки</span>
@@ -27,6 +29,51 @@ const CalcForm = () => {
             выезд<input className="calc-form__date-input" type="date"></input>
           </label>
         </div>
+      </div>
+      <label className="calc-form__date-label">
+        гости
+        <select className="calc-form__select">
+          <option value="" disabled selected>
+            Сколько гостей
+          </option>
+          <option value="1">1 гость</option>
+          <option value="2">2 гостя</option>
+          <option value="2">3 гостя</option>
+        </select>
+      </label>
+      <div className="calc-form__services">
+        <div className="calc-form__services-list">
+          <div className="calc-form__services-text">9 990₽ х 4 суток</div>
+          <div className="calc-form__services-price">39 960₽</div>
+        </div>
+        <div className="calc-form__services-list">
+          <div className="calc-form__services-text">
+            Сбор за услуги: скидка 2 179₽
+          </div>
+          <div className="calc-form__services-items">
+            <div className="calc-form__services-icon">i</div>
+            <div className="calc-form__services-price">0₽</div>
+          </div>
+        </div>
+        <div className="calc-form__services-list">
+          <div className="calc-form__services-text">
+            Сбор за дополнительные услуги
+          </div>
+          <div className="calc-form__services-items">
+            <div className="calc-form__services-icon">i</div>
+            <div className="calc-form__services-price">300₽</div>
+          </div>
+        </div>
+        <div className="calc-form__services-total">
+          <div className="calc-form__services-total-text">Итого</div>
+          <div className="calc-form__services-total-border"></div>
+          <div className="calc-form__services-total-price">38 081₽</div>
+        </div>
+      </div>
+      <div className="calc-form__button">
+        <Button arrow addClass="calc-form__button-arrow">
+          забронировать
+        </Button>
       </div>
     </form>
   );
--- src/components/UI/Button/index.js
+++ src/components/UI/Button/index.js
@@ -1,13 +1,22 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import "./style.scss";
 
 // markup
-const Button = ({ children, secondary, addClass }) => {
+const Button = ({ children, secondary, arrow, addClass }) => {
   return (
     <button
-      className={`button ${addClass} ${secondary ? "button--secondary" : ""}`}
+      className={`button ${addClass} ${secondary ? "button--secondary" : ""} ${
+        arrow ? "button--arrow" : ""
+      }`}
     >
       {children}
+      {arrow && (
+        <div className="button__arrow">
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </div>
+      )}
     </button>
   );
 };
