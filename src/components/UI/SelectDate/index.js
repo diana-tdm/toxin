1a17dc4e296d683b7c0a75bcb33503bf43ef2af3
Diana <sun_ray@mail.ru>
2021-08-11 11:12:27 +0500

    WIP стилизация Air Datepicker

--- src/components/UI/SelectDate/index.js
+++ src/components/UI/SelectDate/index.js
@@ -8,31 +8,61 @@ import "./style.scss";
 const SelectDate = ({ children, secondary, arrow, addClass }) => {
   const arrivalField = React.useRef(null);
   const leaveField = React.useRef(null);
+  const datesField = React.useRef(null);
+
+  function onSelect(formattedDate, date, inst) {
+    const dates = formattedDate.split(",");
+    arrivalField.current.value = dates[0] ?? "";
+    leaveField.current.value = dates[1] ?? "";
+  }
+
+  function toggle() {
+    const datepicker = $(datesField.current).datepicker().data("datepicker");
+    datepicker.show();
+  }
 
   React.useEffect(() => {
-    $(arrivalField.current).datepicker({
+    $(datesField.current).datepicker({
       dateFormat: "dd.mm.yyyy",
       clearButton: true,
+      todayButton: true,
       range: true,
+      inline: true,
+      onSelect,
+      prevHtml: `<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16.1757 8.01562V9.98438H3.98819L9.56632 15.6094L8.16007 17.0156L0.144441 9L8.16007 0.984375L9.56632 2.39062L3.98819 8.01562H16.1757Z" fill="#BC9CFF"/>
+      </svg>
+      `,
+      nextHtml: `<svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M8.36252 0.984375L16.3781 9L8.36252 17.0156L6.95627 15.6094L12.5344 9.98438H0.346894V8.01562H12.5344L6.95627 2.39062L8.36252 0.984375Z" fill="#BC9CFF"/>
+      </svg>
+      `,
     });
   }, []);
   return (
-    <div className="searchform__dates">
-      <div className="searchform__date">
-        <label className="searchform__date-label">
+    <div className="select-date">
+      <div className="select-date__date ">
+        <label className="select-date__date-label">
           прибытие
-          <input ref={arrivalField} className="searchform__date-input"></input>
+          <input
+            ref={arrivalField}
+            className="select-date__date-input"
+            type="text"
+            onClick={toggle}
+          ></input>
         </label>
       </div>
-      <div className="searchform__date">
-        <label className="searchform__date-label">
+      <div className="select-date__date">
+        <label className="select-date__date-label">
           выезд
           <input
             ref={leaveField}
-            className="searchform__date-input"
-            type="date"
+            className="select-date__date-input"
+            type="text"
+            onClick={toggle}
           ></input>
         </label>
+        <input ref={datesField} className="select-date__hidden-input"></input>
       </div>
     </div>
   );
