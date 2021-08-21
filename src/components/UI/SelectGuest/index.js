ccbf59b8b69c8d441531956821b206f5b341a251
Diana <sun_ray@mail.ru>
2021-08-21 13:00:49 +0500

    Настроила работу selector Гости

--- src/components/UI/SelectGuest/index.js
+++ src/components/UI/SelectGuest/index.js
@@ -6,6 +6,9 @@ import "./style.scss";
 // markup
 const SelectGuest = () => {
   const [open, setOpen] = React.useState(false);
+  const [adult, setAdult] = React.useState(0);
+  const [children, setChildren] = React.useState(0);
+  const [infant, setInfant] = React.useState(0);
 
   const handleClick = () => {
     setOpen(!open);
@@ -14,7 +17,11 @@ const SelectGuest = () => {
   return (
     <label className="select-guest">
       гости
-      <div className="select-guest__wrapper">
+      <div
+        className={`select-guest__wrapper ${
+          open ? "select-guest__wrapper--open" : ""
+        }`}
+      >
         <button
           type="button"
           className="select-guest__button"
@@ -25,26 +32,65 @@ const SelectGuest = () => {
         </button>
         <div
           className={`select-guest__dropdown ${
-            open ? "select-guest__dropdown--active" : ""
+            open ? "select-guest__dropdown--open" : ""
           }`}
         >
           <div className="select-guest__dropdown-item">
-            <div className="select-guest__dropdown-item-title">Взролые </div>
-            <button className="select-guest__dropdown-item-button">-</button>
-            <div className="select-guest__dropdown-item-count">2</div>
-            <button className="select-guest__dropdown-item-button">+</button>
+            <div className="select-guest__dropdown-item-title">Взрослые </div>
+            <button
+              type="button"
+              onClick={() => setAdult(adult - 1)}
+              className="select-guest__dropdown-item-button"
+              disabled={adult === 0}
+            >
+              -
+            </button>
+            <div className="select-guest__dropdown-item-count">{adult}</div>
+            <button
+              type="button"
+              onClick={() => setAdult(adult + 1)}
+              className="select-guest__dropdown-item-button"
+            >
+              +
+            </button>
           </div>
           <div className="select-guest__dropdown-item">
             <div className="select-guest__dropdown-item-title">Дети </div>
-            <button className="select-guest__dropdown-item-button">-</button>
-            <div className="select-guest__dropdown-item-count">1</div>
-            <button className="select-guest__dropdown-item-button">+</button>
+            <button
+              type="button"
+              onClick={() => setChildren(children - 1)}
+              className="select-guest__dropdown-item-button"
+              disabled={children === 0}
+            >
+              -
+            </button>
+            <div className="select-guest__dropdown-item-count">{children}</div>
+            <button
+              type="button"
+              onClick={() => setChildren(children + 1)}
+              className="select-guest__dropdown-item-button"
+            >
+              +
+            </button>
           </div>
           <div className="select-guest__dropdown-item">
             <div className="select-guest__dropdown-item-title">Младенцы </div>
-            <button className="select-guest__dropdown-item-button">-</button>
-            <div className="select-guest__dropdown-item-count">0</div>
-            <button className="select-guest__dropdown-item-button">+</button>
+            <button
+              type="button"
+              onClick={() => setInfant(infant - 1)}
+              className="select-guest__dropdown-item-button"
+              disabled={infant === 0}
+            >
+              -
+            </button>
+            <div className="select-guest__dropdown-item-count">{infant}</div>
+            <button
+              type="button"
+              onClick={() => setInfant(infant + 1)}
+              className="select-guest__dropdown-item-button"
+            >
+              +
+            </button>
           </div>
           <div className="select-guest__dropdown-buttons">
             <button className="select-guest__dropdown-button">очистить</button>
