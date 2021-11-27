4a08a41ebb30c1120c16eb25f22399ef4922543d
Tolkacheva Diana <sun_ray@mail.ru>
2021-11-27 17:17:28 +0500

    SearchRoom доделала Дополнительные удобства

--- src/components/UI/Addcomfort/index.js
+++ src/components/UI/Addcomfort/index.js
@@ -1,31 +1,54 @@
 import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import "./style.scss";
 
 // markup
 const Addcomfort = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <div className="addcomfort">
-      <div className="addcomfort__title">дополнительные удобства</div>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Завтрак
-      </label>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Письменный
-        стол
-      </label>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Стул для
-        кормления
-      </label>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Кроватка
-      </label>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Телевизор
-      </label>
-      <label className="addcomfort__label">
-        <input className="addcomfort__input" type="checkbox"></input>Шампунь
-      </label>
+      <button type="button" className="addcomfort__title" onClick={handleClick}>
+        дополнительные удобства{" "}
+        <div
+          className={`addcomfort__icon ${
+            open ? "addcomfort__icon--active" : ""
+          }`}
+        >
+          <FontAwesomeIcon icon={faChevronDown} size="1x" />
+        </div>
+      </button>
+      {open && (
+        <>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>Завтрак
+          </label>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>
+            Письменный стол
+          </label>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>Стул
+            для кормления
+          </label>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>
+            Кроватка
+          </label>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>
+            Телевизор
+          </label>
+          <label className="addcomfort__label">
+            <input className="addcomfort__input" type="checkbox"></input>Шампунь
+          </label>
+        </>
+      )}
     </div>
   );
 };
