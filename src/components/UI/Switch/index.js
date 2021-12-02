7329240d36387379a737df862c42552d126ef56c
Tolkacheva Diana <sun_ray@mail.ru>
2021-12-02 16:17:37 +0500

    Сделала Switch - ползунок Получать спецпредложения

--- src/components/UI/Switch/index.js
+++ src/components/UI/Switch/index.js
@@ -3,10 +3,22 @@ import "./style.scss";
 
 // markup
 const Switch = ({ children }) => {
+  const [active, setActive] = React.useState(false);
+
+  function toggle() {
+    setActive(!active);
+  }
+
   return (
     <label className="switch__label">
-      <button className="switch__border">
-        <div className="switch__circle"></div>
+      <button
+        className={`switch__border ${active ? "switch__border--active" : ""}`}
+        type="button"
+        onClick={toggle}
+      >
+        <div
+          className={`switch__circle ${active ? "switch__circle--active" : ""}`}
+        ></div>
       </button>
       {children}
     </label>
