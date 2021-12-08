b6561d93152265d2ef42dc5a4d2663814161fabb
Tolkacheva Diana <sun_ray@mail.ru>
2021-12-08 15:25:58 +0500

    Сделала мобильную версию Header, сделала бургер и выпадающее меню с кнопками

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,18 +6,25 @@ import "./style.scss";
 
 // markup
 const Header = () => {
+  const [show, setShow] = React.useState(false);
+
+  function onClick() {
+    setShow(!show);
+  }
+
   return (
     <header className="header">
+      <div className="header__overflow"></div>
       <div className="container container__header">
         <Link to="/">
           <img src={Logo} alt="logo-header" />
         </Link>
-        <button class="header__btn">
+        <button class="header__btn" onClick={onClick}>
           <span></span>
         </button>
-        <div className="header__nav">
-          <nav className="header__menu">
-            <ul>
+        <div className={`header__nav ${show ? "" : "header__nav--hide"}`}>
+          <nav>
+            <ul className="header__menu">
               <li className="header__link">
                 <a href="">О нас</a>
               </li>
@@ -36,7 +43,9 @@ const Header = () => {
             </ul>
           </nav>
           <Link to="/login" className="header__mr20">
-            <Button secondary>войти</Button>
+            <Button addClass="header__full-width" secondary>
+              войти
+            </Button>
           </Link>
           <Link to="/registration">
             <Button>зарегистрироваться</Button>
