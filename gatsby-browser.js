9b527f72bd988726cc349d8ee8a1e5ca72a03db4
Tolkacheva Diana <sun_ray@mail.ru>
2021-12-24 14:08:19 +0500

    Переделала шрифты на использование локальных шрифтов

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,4 @@
 import "./src/global.css";
-import "@fontsource/montserrat";
-import "@fontsource/montserrat/700.css";
-import "@fontsource/quicksand";
-import "@fontsource/quicksand/700.css";
 
 import "air-datepicker/dist/css/datepicker.css";
 import "air-datepicker/dist/js/datepicker";
